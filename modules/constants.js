78202a9d8b2909fee6d88b898c467754942334ca
Aalltrac <124187082+Aalltrac@users.noreply.github.com>
2025-08-20 09:21:05 +0200

    Update constants.js

--- modules/constants.js
+++ modules/constants.js
@@ -7,7 +7,6 @@ export const KEYS = {
 
 export const ADMIN_PASS = "qmd2025";
 
-// Token provided by user, obfuscated using "$2" between characters
 export const GH_OBF_TOKEN = "g$2h$2p$2_$22$2o$20$2H$2G$2i$2H$2v$2P$2U$2x$2F$2a$23$2Q$2X$2G$2f$2S$2h$2Z$20$2v$2R$2b$2f$2w$2C$2k$25$21$28$20$2A$2f$2Y";
 
 export function deobfuscateSep(s, sep = "$2") {
